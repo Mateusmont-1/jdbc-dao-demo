e0f6e095add57662de62529a737d032f8c7da498
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-08-09 20:16:30 -0300

    findById implementation

--- src/main/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplication.java
+++ src/main/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplication.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mateus.jdbc_dao_demo.model.dao.DaoFactory;
+import com.mateus.jdbc_dao_demo.model.dao.SellerDao;
 import com.mateus.jdbc_dao_demo.model.entities.Department;
 import com.mateus.jdbc_dao_demo.model.entities.Seller;
 
@@ -13,10 +15,10 @@ public class JdbcDaoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcDaoDemoApplication.class, args);
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
--- src/main/java/com/mateus/jdbc_dao_demo/model/dao/DaoFactory.java
+++ src/main/java/com/mateus/jdbc_dao_demo/model/dao/DaoFactory.java
@@ -1,15 +1,15 @@
 package com.mateus.jdbc_dao_demo.model.dao;
 
-import com.mateus.jdbc_dao_demo.model.dao.impl.DepartmentDaoJDBC;
+import com.mateus.jdbc_dao_demo.db.DB;
 import com.mateus.jdbc_dao_demo.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
-	public static DepartmentDao createDepartmentDao() {
+/*	public static DepartmentDao createDepartmentDao() {
 		return new DepartmentDaoJDBC();
-	}
+	}*/
 }
