13dbaa76509adc971851d7f2270b3853c040bdf3
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-08-09 19:46:28 -0300

    Create Department and Seller entity class

--- src/main/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplication.java
+++ src/main/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplication.java
@@ -1,13 +1,24 @@
 package com.mateus.jdbc_dao_demo;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mateus.jdbc_dao_demo.model.entities.Department;
+import com.mateus.jdbc_dao_demo.model.entities.Seller;
 
 @SpringBootApplication
 public class JdbcDaoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcDaoDemoApplication.class, args);
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
