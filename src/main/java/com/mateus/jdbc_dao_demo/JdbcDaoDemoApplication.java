1ac022ff63b1ee160dadc4eb3f01359e7723d411
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-08-09 19:31:09 -0300

    Created project

---
+++ src/main/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.jdbc_dao_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDaoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDaoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/jdbc_dao_demo/JdbcDaoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.jdbc_dao_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDaoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
